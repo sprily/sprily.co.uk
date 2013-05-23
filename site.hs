271079d88b989cd224b4ae84aa2a52caac9a6afa
Ian Murray <ian.c.murray@gmail.com>
2013-05-23 20:00:37 +0100

    Remove unused blog

--- site.hs
+++ site.hs
@@ -19,25 +19,25 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+    --match "posts/*" $ do
+    --    route $ setExtension "html"
+    --    compile $ pandocCompiler
+    --        >>= loadAndApplyTemplate "templates/post.html"    postCtx
+    --        >>= loadAndApplyTemplate "templates/default.html" postCtx
+    --        >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            let archiveCtx =
-                    field "posts" (\_ -> postList recentFirst) `mappend`
-                    constField "title" "Archives"              `mappend`
-                    defaultContext
+    --create ["archive.html"] $ do
+    --    route idRoute
+    --    compile $ do
+    --        let archiveCtx =
+    --                field "posts" (\_ -> postList recentFirst) `mappend`
+    --                constField "title" "Archives"              `mappend`
+    --                defaultContext
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+    --        makeItem ""
+    --            >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+    --            >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+    --            >>= relativizeUrls
 
 
     match "index.html" $ do
