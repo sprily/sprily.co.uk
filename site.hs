e466f512404a2e73f6a235bdc49b5c5a10beac50
Ian Murray <ian.c.murray@gmail.com>
2014-07-18 11:37:28 +0100

    add bootstrap resources

--- site.hs
+++ site.hs
@@ -7,11 +7,15 @@ import           Hakyll
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
+    match "images/**" $ do
         route   idRoute
         compile copyFileCompiler
 
-    match "css/*" $ do
+    match "javascript/**" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/**" $ do
         route   idRoute
         compile compressCssCompiler
 
